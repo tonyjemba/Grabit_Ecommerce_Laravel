28bce4cb00fdc4b9175019514512901fd6514676
Tony Jemba <jjembatony@gmail.com>
2022-01-31 23:11:00 +0300

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,159 @@
-<x-guest-layout>
+@extends('Frontend.main_master')
+
+@section('content')
+
+<div class="breadcrumb">
+	<div class="container">
+		<div class="breadcrumb-inner">
+			<ul class="list-inline list-unstyled">
+				<li><a href="home.html">Home</a></li>
+				<li class='active'>Login</li>
+			</ul>
+		</div><!-- /.breadcrumb-inner -->
+	</div><!-- /.container -->
+</div><!-- /.breadcrumb -->
+<div class="body-content">
+    <div class="container">
+      <div class="sign-in-page">
+        <div class="row">
+          <!-- Sign-in -->
+          <div class="col-md-6 col-sm-6 sign-in">
+            <h4 class="">Sign in</h4>
+            <p class="">Hello, Welcome to your account.</p>
+            <div class="social-sign-in outer-top-xs">
+              <a href="#" class="facebook-sign-in"
+                ><i class="fa fa-facebook"></i> Sign In with Facebook</a
+              >
+              <a href="#" class="twitter-sign-in"
+                ><i class="fa fa-twitter"></i> Sign In with Twitter</a
+              >
+            </div>
+            <form method="POST" action="{{ isset($guard)?url($guard.'/login'):route('login') }}" class="register-form outer-top-xs" role="form">
+                @csrf
+              <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1"
+                  >Email Address <span>*</span></label
+                >
+                <input
+                  type="email"
+                  class="form-control unicase-form-control text-input"
+                  id="email"
+                  name="email"
+                />
+              </div>
+              <div class="form-group">
+                <label class="info-title" for="exampleInputPassword1"
+                  >Password <span>*</span></label
+                >
+                <input
+                  type="password"
+                  class="form-control unicase-form-control text-input"
+                  id="password"
+                  name="password"
+                />
+              </div>
+              <div class="radio outer-xs">
+                <label>
+                  <input
+                    type="radio"
+                    name="optionsRadios"
+                    id="optionsRadios2"
+                    value="option2"
+                  />Remember me!
+                </label>
+                <a href="{{ route('password.request') }}" class="forgot-password pull-right"
+                  >Forgot your Password?</a
+                >
+              </div>
+              <button
+                type="submit"
+                class="btn-upper btn btn-primary checkout-page-button"
+              >
+                Login
+              </button>
+            </form>
+          </div>
+          <!-- Sign-in -->
+
+          <!-- create a new account -->
+          <div class="col-md-6 col-sm-6 create-new-account">
+            <h4 class="checkout-subtitle">Create a new account</h4>
+            <p class="text title-tag-line">Create your new account.</p>
+            <form class="register-form outer-top-xs" role="form">
+              <div class="form-group">
+                <label class="info-title" for="exampleInputEmail2"
+                  >Email Address <span>*</span></label
+                >
+                <input
+                  type="email"
+                  class="form-control unicase-form-control text-input"
+                  id="exampleInputEmail2"
+                />
+              </div>
+              <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1"
+                  >Name <span>*</span></label
+                >
+                <input
+                  type="email"
+                  class="form-control unicase-form-control text-input"
+                  id="exampleInputEmail1"
+                />
+              </div>
+              <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1"
+                  >Phone Number <span>*</span></label
+                >
+                <input
+                  type="email"
+                  class="form-control unicase-form-control text-input"
+                  id="exampleInputEmail1"
+                />
+              </div>
+              <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1"
+                  >Password <span>*</span></label
+                >
+                <input
+                  type="email"
+                  class="form-control unicase-form-control text-input"
+                  id="exampleInputEmail1"
+                />
+              </div>
+              <div class="form-group">
+                <label class="info-title" for="exampleInputEmail1"
+                  >Confirm Password <span>*</span></label
+                >
+                <input
+                  type="email"
+                  class="form-control unicase-form-control text-input"
+                  id="exampleInputEmail1"
+                />
+              </div>
+              <button
+                type="submit"
+                class="btn-upper btn btn-primary checkout-page-button"
+              >
+                Sign Up
+              </button>
+            </form>
+          </div>
+          <!-- create a new account -->
+        </div>
+        <!-- /.row -->
+      </div>
+      <!-- /.sigin-in-->
+      <!-- ============================================== BRANDS CAROUSEL ============================================== -->
+      @include('Frontend.Body.brand')
+      <!-- /.logo-slider -->
+      <!-- ============================================== BRANDS CAROUSEL : END ============================================== -->
+    </div>
+    <!-- /.container -->
+  </div>
+
+@endsection
+
+{{-- <x-guest-layout>
     <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
@@ -45,4 +200,4 @@
             </div>
         </form>
     </x-jet-authentication-card>
-</x-guest-layout>
+</x-guest-layout> --}}
