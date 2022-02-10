70f5e184e28b800ac053b080dda068726349a013
Tony Jemba <jjembatony@gmail.com>
2022-02-10 13:13:54 +0300

    adding slider content

--- app/Http/Controllers/backend/SliderController.php
+++ app/Http/Controllers/backend/SliderController.php
@@ -5,11 +5,42 @@ namespace App\Http\Controllers\backend;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
+use Image;
 
 class SliderController extends Controller
 {
     public function SliderView(){
         $sliders = Slider::latest()->get();
 		return view('backend.slider.slider_view',compact('sliders'));
+    }
+
+    public function SliderStore(Request $request){
+        
+    	$request->validate([
+    		 
+    		'slider_img' => 'required',
+    	],[
+    		'slider_img.required' => 'Please Select One Image',
+    		 
+    	]);
+
+    	$image = $request->file('slider_img');
+    	$name_gen = hexdec(uniqid()).'.'.$image->getClientOriginalExtension();
+    	Image::make($image)->resize(870,370)->save('upload/slider/'.$name_gen);
+    	$save_url = 'upload/slider/'.$name_gen;
+
+	Slider::insert([
+		'title' => $request->title,
+		'description' => $request->description,
+		'slider_img' => $save_url,
+
+    	]);
+
+	    $notification = array(
+			'message' => 'Slider Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->back()->with($notification);
     }
 }
