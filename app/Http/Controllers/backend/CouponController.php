d73eed86b6a7fbdb5fa2b6907b86bc2ef0dddbfa
Tony Jemba <jjembatony@gmail.com>
2022-02-21 13:31:13 +0300

    added coupon add functionality

--- app/Http/Controllers/backend/CouponController.php
+++ app/Http/Controllers/backend/CouponController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use App\Models\Coupon;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CouponController extends Controller
@@ -12,6 +13,34 @@ class CouponController extends Controller
     public function CouponView(){
     	$coupons = Coupon::orderBy('id','DESC')->get();
     	return view('backend.coupon.view_coupon',compact('coupons'));
+
+    }
+
+    public function CouponStore(Request $request){
+
+    	$request->validate([
+    		'coupon_name' => 'required',
+    		'coupon_discount' => 'required',
+    		'coupon_validity' => 'required',
+    	 
+    	]);
+
+    	 
+
+	Coupon::insert([
+		'coupon_name' => strtoupper($request->coupon_name),
+		'coupon_discount' => $request->coupon_discount, 
+		'coupon_validity' => $request->coupon_validity,
+		'created_at' => Carbon::now(),
+
+    	]);
+
+	    $notification = array(
+			'message' => 'Coupon Inserted Successfully',
+			'alert-type' => 'success'
+		);
+
+		return redirect()->back()->with($notification);
 
     }
 }
