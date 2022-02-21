97f4dd79e4a248c817315214fb4cd5fd74e7a9cd
Tony Jemba <jjembatony@gmail.com>
2022-02-21 14:10:10 +0300

    shiping division update  page functionality

--- app/Http/Controllers/backend/ShippingAreaController.php
+++ app/Http/Controllers/backend/ShippingAreaController.php
@@ -9,31 +9,56 @@ use Illuminate\Http\Request;
 
 class ShippingAreaController extends Controller
 {
-    public function DivisionView(){
-		$divisions = ShipDivision::orderBy('id','DESC')->get();
-		return view('backend.ship.division.view_division',compact('divisions'));
+    public function DivisionView()
+    {
+        $divisions = ShipDivision::orderBy('id', 'DESC')->get();
+        return view('backend.ship.division.view_division', compact('divisions'));
+    }
 
-	}
+    public function DivisionStore(Request $request)
+    {
 
-    public function DivisionStore(Request $request){
+        $request->validate([
+            'division_name' => 'required',
 
-    	$request->validate([
-    		'division_name' => 'required',   	 
-    	 
-    	]);
+        ]);
         ShipDivision::insert([
-	 
+
             'division_name' => $request->division_name,
             'created_at' => Carbon::now(),
-    
-            ]);
-    
-            $notification = array(
-                'message' => 'Division Inserted Successfully',
-                'alert-type' => 'success'
-            );
-    
-            return redirect()->back()->with($notification);
-    
-        }
+
+        ]);
+
+        $notification = array(
+            'message' => 'Division Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+    }
+
+    public function DivisionEdit($id)
+    {
+
+        $divisions = ShipDivision::findOrFail($id);
+        return view('backend.ship.division.edit_division', compact('divisions'));
+    }
+
+    public function DivisionUpdate(Request $request, $id)
+    {
+
+        ShipDivision::findOrFail($id)->update([
+
+            'division_name' => $request->division_name,
+            'created_at' => Carbon::now(),
+
+        ]);
+
+        $notification = array(
+            'message' => 'Division Updated Successfully',
+            'alert-type' => 'info'
+        );
+
+        return redirect()->route('manage-division')->with($notification);
+    }
 }
--- app/Models/ShipDivision.php
+++ app/Models/ShipDivision.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
 class ShipDivision extends Model
 {
     use HasFactory;
-    protected $guaded=[];
+    protected $guarded = [];
 }
