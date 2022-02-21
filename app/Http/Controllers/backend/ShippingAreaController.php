2b57ac67b6722f2d638777e5b0e27b87ad7ef759
Tony Jemba <jjembatony@gmail.com>
2022-02-21 14:01:46 +0300

    shiping division add page

--- app/Http/Controllers/backend/ShippingAreaController.php
+++ app/Http/Controllers/backend/ShippingAreaController.php
@@ -3,9 +3,14 @@
 namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
+use App\Models\ShipDivision;
 use Illuminate\Http\Request;
 
 class ShippingAreaController extends Controller
 {
-    //
+    public function DivisionView(){
+		$divisions = ShipDivision::orderBy('id','DESC')->get();
+		return view('backend.ship.division.view_division',compact('divisions'));
+
+	}
 }
