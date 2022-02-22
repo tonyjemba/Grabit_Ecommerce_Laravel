20b8050b0dd71d9e75a54999114f627885ea5f1f
Tony Jemba <jjembatony@gmail.com>
2022-02-22 18:42:33 +0300

    checkout store function updated

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -24,26 +24,19 @@ class CheckoutController extends Controller
         return json_encode($ship);
     }
 
-    public function CheckoutStore(Request $request){
-    	// dd($request->all());
-    	$data = array();
-    	$data['shipping_name'] = $request->shipping_name;
-    	$data['shipping_email'] = $request->shipping_email;
-    	$data['shipping_phone'] = $request->shipping_phone;
-    	$data['post_code'] = $request->post_code;
-    	$data['division_id'] = $request->division_id;
-    	$data['district_id'] = $request->district_id;
-    	$data['state_id'] = $request->state_id;
-    	$data['notes'] = $request->notes;
-    	$cartTotal = Cart::total();
-        if ($request->payment_method == 'stripe') {
-    		return view('frontend.payment.stripe',compact('data','cartTotal'));
-    	}elseif ($request->payment_method == 'card') {
-    		return 'card';
-    	}else{
-            return view('frontend.payment.cash',compact('data','cartTotal'));
-    	}
-    	 
-
+    public function CheckoutStore(Request $request)
+    {
+        // dd($request->all());
+        $data = array();
+        $data['shipping_name'] = $request->shipping_name;
+        $data['shipping_email'] = $request->shipping_email;
+        $data['shipping_phone'] = $request->shipping_phone;
+        $data['post_code'] = $request->post_code;
+        $data['division_id'] = $request->division_id;
+        $data['district_id'] = $request->district_id;
+        $data['state_id'] = $request->state_id;
+        $data['noted'] = $request->notes;
+        $cartTotal = Cart::total();
+     
     }
 }
