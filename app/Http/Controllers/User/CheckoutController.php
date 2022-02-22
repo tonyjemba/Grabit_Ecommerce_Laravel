cfe30ce16164c64f414a74018dedbd53d87ab8fc
Tony Jemba <jjembatony@gmail.com>
2022-02-22 18:50:01 +0300

    stripe payment page

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -37,6 +37,12 @@ class CheckoutController extends Controller
         $data['state_id'] = $request->state_id;
         $data['noted'] = $request->notes;
         $cartTotal = Cart::total();
-     
+        if ($request->payment_method == 'stripe') {
+            return view('frontend.payment.stripe', compact('data', 'cartTotal'));
+        } elseif ($request->payment_method == 'card') {
+            return 'card';
+        } else {
+            return view('frontend.payment.cash', compact('data', 'cartTotal'));
+        }
     }
 }
