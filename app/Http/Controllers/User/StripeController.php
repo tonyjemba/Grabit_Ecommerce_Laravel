f198e1bdde4c06a61f9473750665c3f02872bec2
Tony Jemba <jjembatony@gmail.com>
2022-02-25 19:20:51 +0300

    adding payment data to tables

--- app/Http/Controllers/User/StripeController.php
+++ app/Http/Controllers/User/StripeController.php
@@ -3,25 +3,64 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Cartalyst\Stripe\Stripe;
+use Gloudemans\Shoppingcart\Facades\Cart;
+use Illuminate\Contracts\Session\Session;
+use Illuminate\Support\Facades\Auth;
 
 class StripeController extends Controller
 {
+
+	
 	public function StripeOrder(Request $request){
 		\Stripe\Stripe::setApiKey(env('STRIPE_API_KEY'));
 
-	 
+		if (Session::has('coupon')) {
+    		$total_amount = Session::get('coupon')['total_amount'];
+    	}else{
+    		$total_amount = round(Cart::total());
+    	}
 	$token = $_POST['stripeToken'];
 	$charge = \Stripe\Charge::create([
-	  'amount' =>100,
+	  'amount' =>$total_amount * 100,
 	  'currency' => 'usd',
-	  'description' => 'Easy Online Store',
+	  'description' => 'Grabit Ecommerce solutions',
 	  'source' => $token,
 	  'metadata' => ['order_id' => uniqid()],
 	]);
 
-	dd($charge);
+	$order_id = Order::insertGetId([
+		'user_id' => Auth::id(),
+		'division_id' => $request->division_id,
+		'district_id' => $request->district_id,
+		'state_id' => $request->state_id,
+		'name' => $request->name,
+		'email' => $request->email,
+		'phone' => $request->phone,
+		'post_code' => $request->post_code,
+		'notes' => $request->notes,
+
+		'payment_type' => 'Stripe',
+		'payment_method' => 'Stripe',
+		'payment_type' => $charge->payment_method,
+		'transaction_id' => $charge->balance_transaction,
+		'currency' => $charge->currency,
+		'amount' => $total_amount,
+		'order_number' => $charge->metadata->order_id,
+
+		'invoice_no' => 'EOS'.mt_rand(10000000,99999999),
+		'order_date' => Carbon::now()->format('d F Y'),
+		'order_month' => Carbon::now()->format('F'),
+		'order_year' => Carbon::now()->format('Y'),
+		'status' => 'pending',
+		'created_at' => Carbon::now(),	 
+
+	]);
+
+
 	}
 
 
