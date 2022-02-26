e801388cc1f9518f099e3e460367f122de3af745
Tony Jemba <jjembatony@gmail.com>
2022-02-26 21:13:12 +0300

    adding

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,16 +5,7 @@
 <div class="body-content">
     <div class="container">
         <div class="row">
-            <div class="col-md-2">
-                <img class="img-card-top " height="100%" width="100%" style=" border-radius:50%"  src="{{ !empty(Auth::user()->profile_photo_path)? url('upload/user_images/'.Auth::user()->profile_photo_path):url('upload/no_image.jpg') }}" alt="User Avatar">
-                <br>
-                <ul class="list-group list-group-flush">
-                    <a href="" class="btn btn-primary btn-sm btn-block " style="margin-top:20px">Home</a>
-                    <a href="{{ route('profile.update') }}" class="btn btn-primary btn-sm btn-block">Profile Update</a>
-                    <a href="{{route( "change.password") }}" class="btn btn-primary btn-sm btn-block">Change Password</a>
-                    <a href="{{ route('user.logout') }}" class="btn btn-danger btn-sm btn-block">Logout</a>
-                </ul>
-            </div>
+            @include('frontend.common.user_sidebar')
             <div class="col-md-2">
                 
             </div>
