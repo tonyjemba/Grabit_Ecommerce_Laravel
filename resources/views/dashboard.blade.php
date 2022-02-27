5d1ea3affb8767c7c3c97c82d6e478952fff2661
Tony Jemba <jjembatony@gmail.com>
2022-02-27 23:17:31 +0300

    final2

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -30,15 +30,3 @@
 </div>
 @endsection
 
-
-{{-- <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-           Hi..{{ Auth::user()->name }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        This is just home page 
-    </div>
-</x-app-layout> --}}
